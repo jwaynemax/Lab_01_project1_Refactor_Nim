91744e315d0e9f3352032dc76370bc8ad88b2449
jm00724 <jm00724@csvlab-vm07.uwg.westga.edu>
2023-06-09 17:33:31 -0400

    edu.westga.cs6910.nim.test package created and PileWhenCreatePile created

---
+++ src/edu/westga/cs6910/nim/test/PileWhenCreatePile.java
@@ -0,0 +1,14 @@
+package edu.westga.cs6910.nim.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PileWhenCreatePile {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
