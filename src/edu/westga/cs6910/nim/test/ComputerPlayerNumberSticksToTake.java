2b2e13f4021a720466d04efc80ac481097e823d6
Justin Maxwell <jm00724@my.westga.edu>
2023-06-12 13:34:57 -0400

    ComputerPlayerNumberSticksToTake junit tests validated

--- src/edu/westga/cs6910/nim/test/ComputerPlayerNumberSticksToTake.java
+++ src/edu/westga/cs6910/nim/test/ComputerPlayerNumberSticksToTake.java
@@ -4,11 +4,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs6910.nim.model.ComputerPlayer;
+
 class ComputerPlayerNumberSticksToTake {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testSetNumberSticksToTakeNoParam() {
+		ComputerPlayer computer = new ComputerPlayer();	
+		computer.setNumberSticksToTake();
+		assertEquals(1, computer.getSticksToTake());
+	}
+	
+	@Test
+	void testSetNumberSticksToTakeWithParam() {
+		ComputerPlayer computer = new ComputerPlayer();	
+		computer.setNumberSticksToTake(3);
+		assertEquals(3, computer.getSticksToTake());
+	}
+	
+	@Test
+	void testSetNumberSticksToTakeLargeNumber() {
+		ComputerPlayer computer = new ComputerPlayer();	
+		computer.setNumberSticksToTake(1000);
+		assertEquals(1000, computer.getSticksToTake());
 	}
 
 }
