8738d7f3267fe38c157a484a8e10a2d869d4f533
Justin Maxwell <jm00724@my.westga.edu>
2023-06-13 17:00:37 -0400

    stopping point

--- src/edu/westga/cs6910/nim/test/GameWhenGetIsGameOver.java
+++ src/edu/westga/cs6910/nim/test/GameWhenGetIsGameOver.java
@@ -4,13 +4,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs6910.nim.model.ComputerPlayer;
 import edu.westga.cs6910.nim.model.Game;
+import edu.westga.cs6910.nim.model.HumanPlayer;
+import edu.westga.cs6910.nim.model.Pile;
 
 class GameWhenGetIsGameOver {
 
 	@Test
 	void  testBeforeGameStartsIsNotOver() {
-		//Game newGame = new Game(null, null);
+		ComputerPlayer computer = new ComputerPlayer();
+		HumanPlayer human = new HumanPlayer("Human");
+		Game game = new Game(human, computer);
+		
+		assertEquals(false, game.isGameOver());		
+	}
+	
+	@Test
+	void  testGameIsOver() {
+		ComputerPlayer computer = new ComputerPlayer();
+		HumanPlayer human = new HumanPlayer("Human");
+		Game game = new Game(human, computer);
+		Pile pile = new Pile(0);
+		
+		computer.setPileForThisTurn(pile);
+		
+		System.out.println(game.getPile());
+		
+		assertEquals(true, game.isGameOver());		
 	}
 
 }
