f50e6acd649ae19c1781e74ea3c129af2b7445ca
Justin Maxwell <jm00724@my.westga.edu>
2023-06-13 20:13:48 -0400

    GameWhenPlay unit tests completed and validated

---
+++ src/edu/westga/cs6910/nim/test/GameWhenPlay.java
@@ -0,0 +1,58 @@
+package edu.westga.cs6910.nim.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.nim.model.ComputerPlayer;
+import edu.westga.cs6910.nim.model.Game;
+import edu.westga.cs6910.nim.model.HumanPlayer;
+
+class GameWhenPlay {
+
+	@Test
+	void testGetCurrentPlayerAfterPlay() {
+		HumanPlayer human = new HumanPlayer("Human");
+		ComputerPlayer computer = new ComputerPlayer();
+		Game game = new Game(human, computer);
+		
+		
+		game.startNewGame(computer);
+		computer.setPileForThisTurn(game.getPile());
+		computer.setSticksToTake(2);
+		game.play();
+		
+		assertEquals("Human", game.getCurrentPlayer().getName());		
+	}
+	
+	@Test
+	void testGetSticksLeftAfterPlay() {
+		HumanPlayer human = new HumanPlayer("Human");
+		ComputerPlayer computer = new ComputerPlayer();
+		Game game = new Game(human, computer);
+		
+		
+		game.startNewGame(computer);
+		computer.setPileForThisTurn(game.getPile());
+		computer.setSticksToTake(2);
+		game.play();
+		
+		assertEquals(5, game.getSticksLeft());		
+	}
+	
+	@Test
+	void testToStringAfterPlay() {
+		HumanPlayer human = new HumanPlayer("Human");
+		ComputerPlayer computer = new ComputerPlayer();
+		Game game = new Game(human, computer);
+		
+		
+		game.startNewGame(computer);
+		computer.setPileForThisTurn(game.getPile());
+		computer.setSticksToTake(2);
+		game.play();
+		
+		assertEquals(" Pile size: 5", game.toString());		
+	}
+
+}
