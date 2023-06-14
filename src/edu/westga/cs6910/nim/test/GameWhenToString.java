23f295ab975a5a0c252325d5c785ab997ae7a54f
Justin Maxwell <jm00724@my.westga.edu>
2023-06-13 20:35:07 -0400

    GameWhenToString unit tests completed and validated

---
+++ src/edu/westga/cs6910/nim/test/GameWhenToString.java
@@ -0,0 +1,58 @@
+package edu.westga.cs6910.nim.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.nim.model.ComputerPlayer;
+import edu.westga.cs6910.nim.model.Game;
+import edu.westga.cs6910.nim.model.HumanPlayer;
+
+class GameWhenToString {
+
+	@Test
+	void testToStringAfterPlay() {
+		HumanPlayer human = new HumanPlayer("Human");
+		ComputerPlayer computer = new ComputerPlayer();
+		Game game = new Game(human, computer);
+		
+		
+		game.startNewGame(computer);
+		computer.setPileForThisTurn(game.getPile());
+		computer.setSticksToTake(2);
+		game.play();
+		
+		assertEquals(" Pile size: 5", game.toString());		
+	}
+	
+	@Test
+	void  testGameIsOverToString() {
+		HumanPlayer human = new HumanPlayer("Human");
+		ComputerPlayer computer = new ComputerPlayer();
+		Game game = new Game(human, computer);
+		
+		
+		game.startNewGame(computer);
+		computer.setPileForThisTurn(game.getPile());
+		computer.setSticksToTake(6);
+		game.play();		
+				
+		assertEquals("Game over! Winner: Simple computer", game.toString());		
+	}
+	
+	@Test
+	void  testToStringWith2SticksLeft() {
+		HumanPlayer human = new HumanPlayer("Human");
+		ComputerPlayer computer = new ComputerPlayer();
+		Game game = new Game(human, computer);
+		
+		
+		game.startNewGame(computer);
+		computer.setPileForThisTurn(game.getPile());
+		computer.setSticksToTake(5);
+		game.play();		
+				
+		assertEquals(" Pile size: 2", game.toString());		
+	}
+
+}
